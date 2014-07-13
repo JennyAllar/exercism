11db84f180230d3ebe88b68a32b9526bcd0e1830
Jenny Allar <JennyAllar@gmail.com>
2014-07-13 17:51:58 -0600

    Solved gigasecond with somewhat foggy understanding of UTC.

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,12 +1,13 @@
+require 'date'
+require 'time'
+
 class Gigasecond
-
-    def initialize(day_born)
-      @starting_time = day_born.to_time
-    end
-
-    def date
-      giga_date = @starting_time + (10**9)
-      return giga_date.to_date
-    end
-
+  def initialize(date)
+    @date = date
+  end
+  
+  def date
+    (@date.to_time + (10**9)).utc.to_date
+  end
+  
 end
