ca07b13efbe990d7685625f791f1da7785c7b1fa
Jenny Allar <JennyAllar@gmail.com>
2014-07-19 21:26:29 -0400

    Set up gigasecond constant per Ben's suggestion.

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -2,12 +2,14 @@ require 'date'
 require 'time'
 
 class Gigasecond
+  GIGASECOND  = 10**9
+
   def initialize(date)
     @date = date
   end
   
   def date
-    (@date.to_time + (10**9)).utc.to_date
+    (@date.to_time + (GIGASECOND)).utc.to_date
   end
   
 end
