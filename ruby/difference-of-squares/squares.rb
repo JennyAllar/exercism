d8ae4a55c205adc68a336418a7fa195b3e65f812
Jenny Allar <JennyAllar@gmail.com>
2014-07-20 22:38:04 -0400

    Completely refactored that atrocious mess of differences-of-squares.

--- ruby/difference-of-squares/squares.rb
+++ ruby/difference-of-squares/squares.rb
@@ -1,40 +1,15 @@
 class Squares
   def initialize(number)
-    @number = number
+    @numbers = 1..number
   end
 
   def square_of_sums
-    array = []
-    new_array = []
-    counter = 1
-    array << @number
-    new_array << @number
-    while counter < @number
-      array.each do |number|
-        new_array << (number - counter)
-        counter += 1
-      end
-    end
-    new_array.inject { |sum, number| sum + number } ** 2
+    total = @numbers.reduce(:+)
+    total ** 2
   end
 
   def sum_of_squares
-    array = []
-    new_array = []
-    counter = 1
-    array << @number
-    new_array << @number
-    while counter < @number
-      array.each do |number|
-        new_array << (number - counter)
-        counter += 1
-      end
-    end
-    total_sum = 0
-    new_array.each do |number|
-      total_sum += (number ** 2)
-    end
-    total_sum
+    @numbers.reduce { |total, x| total + (x * x) }
   end
   
   def difference
