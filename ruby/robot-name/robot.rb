7900eb2021997bbb61d069f2730eda4a08590eff
Jenny Allar <JennyAllar@gmail.com>
2014-07-22 21:38:24 -0600

    Refactored robot-name per Ben's suggestion. Attr_reader used.

--- ruby/robot-name/robot.rb
+++ ruby/robot-name/robot.rb
@@ -1,16 +1,14 @@
 class Robot
+  
+  attr_reader :name
   def initialize
     @name = ""
     2.times{@name << (65 + rand(25)).chr}
     @name.concat(rand(9999).to_s)
   end
   
-  def name
-    @name
-  end
-  
   def reset
-    @name = Robot.new.name
+    initialize
   end
   
   
